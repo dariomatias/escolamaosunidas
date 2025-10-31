58b58dcfdbabe7f504b7cb1339401a6a65a73433
Dario Piriz <piriz.dario@gmail.com>
2025-10-31 10:13:30 -0300

    initial commit.

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import EscolaMaosUnidasSite from './site.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <EscolaMaosUnidasSite />
+  </React.StrictMode>
+)
---
+++ src/site.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+export default function EscolaMaosUnidasSite() {
+  return (
+    <div className='text-center p-10'>
+      <h1 className='text-4xl font-bold mb-4'>Escola Mãos Unidas</h1>
+      <p className='text-lg text-neutral-700'>Sitio institucional - Lichinga, Mozambique</p>
+      <p className='mt-4'>Próximamente contenido completo...</p>
+    </div>
+  )
+}
